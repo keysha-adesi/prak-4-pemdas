cbffeeb1d4b7c97febae4303fcee6179c73bb30e
keysha-adesi <keyshaputrii590@gmail.com>
2026-03-31 22:09:09 +0700

    first commit

---
+++ pemdas/tugaslooping.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+using namespace std;
+
+// Variabel global
+int n, pilihan;
+
+// Function cek bilangan prima
+bool isPrima(int n) {
+    if (n <= 1) return false;
+
+    int i = 2;
+    while (i <= n / 2) {
+        if (n % i == 0) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+
+// Function cek bilangan Fibonacci
+bool isFibonacci(int n) {
+    int a = 0, b = 1, c;
+
+    while (b < n) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return (n == 0 || b == n);
+}
+
+// Prosedur input angka
+void inputAngka() {
+    cout << "Masukkan angka: ";
+    cin >> n;
+}
+
+// Prosedur tampil hasil
+void tampilPrima() {
+    if (isPrima(n))
+        cout << n << " adalah bilangan prima\n";
+    else
+        cout << n << " bukan bilangan prima\n";
+}
+
+void tampilFibonacci() {
+    if (isFibonacci(n))
+        cout << n << " termasuk bilangan Fibonacci\n";
+    else
+        cout << n << " bukan bilangan Fibonacci\n";
+}
+
+// Function menu
+void menu() {
+    cout << "\n=== MENU ===\n";
+    cout << "1. Cek Bilangan Prima\n";
+    cout << "2. Cek Bilangan Fibonacci\n";
+    cout << "0. Keluar\n";
+    cout << "Pilih: ";
+    cin >> pilihan;
+}
+
+// Main program
+int main() {
+    while (true) {
+        menu();
+
+        switch (pilihan) {
+            case 1:
+                inputAngka();
+                tampilPrima();
+                break;
+
+            case 2:
+                inputAngka();
+                tampilFibonacci();
+                break;
+
+            case 0:
+                cout << "Program selesai.\n";
+                return 0;
+
+            default:
+                cout << "Pilihan tidak valid!\n";
+        }
+    }
+}
